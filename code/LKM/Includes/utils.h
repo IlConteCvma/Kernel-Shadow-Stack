2b8c5392bac4857e4edb4a2bdff4940c1b2770ba
conte_cvma <mar.calavaro@gmail.com>
2024-05-29 15:16:10 +0200

    logging

--- code/LKM/Includes/utils.h
+++ code/LKM/Includes/utils.h
@@ -9,6 +9,11 @@
 #define DEBUG
 #endif
 
+#ifdef DEBUG_LOG
+#define DEBUG
+#endif
+
+
 #define DEBUG //TODO remove
 //TODO change includes !!
 #ifdef DEBUG
@@ -22,17 +27,28 @@
 // more verbose function
 #ifdef DEBUG_TEST
 #define dprint_info_test(...) (pr_info(__VA_ARGS__))
+#else
+#define dprint_info_test(...)
 #endif
 
 //verbose level
 #ifdef DEBUG_V
 #define dprint_info_v(...) (pr_info(__VA_ARGS__))
+#else
+#define dprint_info_v(...)
+#endif
+
+#ifdef DEBUG_LOG
+#define dprint_info_log(...) (pr_info(__VA_ARGS__))
+#else
+#define dprint_info_log(...)
 #endif
 
 #else
 #define dprint_info(...)
 #define dprint_info_test(...)
 #define dprint_info_v(...) 
+#define dprint_info_log(...)
 #endif
 
 #endif
